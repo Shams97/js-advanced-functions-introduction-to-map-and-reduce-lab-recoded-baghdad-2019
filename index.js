c9ac426c9cf4f196ce8143de4e5f3f34c81675df
shams khalid <shamsnaji4@gmail.com>
2020-01-06 19:04:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,43 @@ function mapToNoChange(src){
   
   
 }
-function mapToDouble(src){
   
-}
-function mapToSquare(src){}
 
-function reduceToTotal(sourceArray, startingPoint){}
-function reduceToAllTrue(sourceArray){}
-function reduceToAnyTrue(sourceArray){}
+
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i])
+  }
+  return r
+}
+
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i])
+  }
+  return r
+}
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
