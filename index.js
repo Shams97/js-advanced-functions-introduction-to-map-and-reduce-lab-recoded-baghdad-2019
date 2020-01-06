c3b20cf1e4a5c9da17657ca2f06bf297da990763
shams khalid <shamsnaji4@gmail.com>
2020-01-06 19:00:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Your code here
-function mapToNegativize(sourceArray){}
+function mapToNegativize(sourceArray){
+  let negativeTransform= sourceArray.map((negativeTransform)=>{
+    negativeTransform =negativeTransform;
+  })
+  return negativeTransform;
+}
 function mapToNoChange(sourceArray){}
 function mapToDouble(sourceArray){}
 function mapToSquare(sourceArray){}
